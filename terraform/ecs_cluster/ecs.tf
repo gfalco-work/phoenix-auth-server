99f6634422d6d9a5f9e9f86a84476ffe058fc65a
gfalco77 <giuseppefalco77@gmail.com>
2024-04-25 09:03:13 +0100

    added alb

--- terraform/ecs_cluster/ecs.tf
+++ terraform/ecs_cluster/ecs.tf
@@ -61,35 +61,35 @@ resource "aws_ecs_task_definition" "default" {
 #
 # ## Creates ECS Service
 #
-# resource "aws_ecs_service" "service" {
-#   name                               = "${var.namespace}_ECS_Service_${var.environment}"
-#   iam_role                           = aws_iam_role.ecs_service_role.arn
-#   cluster                            = aws_ecs_cluster.default.id
-#   task_definition                    = aws_ecs_task_definition.default.arn
-#   desired_count                      = var.ecs_task_desired_count
-#   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
-#   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
-#
-#   load_balancer {
-#     target_group_arn = aws_alb_target_group.service_target_group.arn
-#     container_name   = var.service_name
-#     container_port   = var.container_port
-#   }
-#
-#   ## Spread tasks evenly accross all Availability Zones for High Availability
-#   ordered_placement_strategy {
-#     type  = "spread"
-#     field = "attribute:ecs.availability-zone"
-#   }
-#
-#   ## Make use of all available space on the Container Instances
-#   ordered_placement_strategy {
-#     type  = "binpack"
-#     field = "memory"
-#   }
-#
-#   ## Do not update desired count again to avoid a reset to this number on every deployment
-#   lifecycle {
-#     ignore_changes = [desired_count]
-#   }
-# }
+resource "aws_ecs_service" "service" {
+  name                               = "${var.namespace}_ECS_Service_${var.environment}"
+  iam_role                           = aws_iam_role.ecs_service_role.arn
+  cluster                            = aws_ecs_cluster.default.id
+  task_definition                    = aws_ecs_task_definition.default.arn
+  desired_count                      = var.ecs_task_desired_count
+  deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
+  deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.service_target_group.arn
+    container_name   = var.service_name
+    container_port   = var.container_port
+  }
+
+  ## Spread tasks evenly accross all Availability Zones for High Availability
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  ## Make use of all available space on the Container Instances
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "memory"
+  }
+
+  ## Do not update desired count again to avoid a reset to this number on every deployment
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
